1bbc44742ad2858eebdcc9b40dd7a2fe2bed8e7f
Sascha Andres <sascha.andres@outlook.com>
2017-01-06 16:58:28 +0100

    Added a first server implementation

---
+++ server/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"fmt"
+
+	pb "github.com/sascha-andres/go-logsink/logsink"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct{}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SendLine(ctx context.Context, in *pb.LineMessage) (*pb.LineResult, error) {
+	fmt.Println(in.Line)
+	return &pb.LineResult{Result: true}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterLogTransferServer(s, &server{})
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
