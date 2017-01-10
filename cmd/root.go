c1b37281f293540ac95ad148ccedbafa090f2848
Sascha Andres <sascha.andres@outlook.com>
2017-01-10 21:06:51 +0100

    Allowing directory config

--- cmd/root.go
+++ cmd/root.go
@@ -64,6 +64,7 @@ func initConfig() {
 
 	viper.SetConfigName(".go-logsink") // name of config file (without extension)
 	viper.AddConfigPath("$HOME")       // adding home directory as first search path
+	viper.AddConfigPath(".")           // look in currect directory for config
 	viper.AutomaticEnv()               // read in environment variables that match
 
 	// If a config file is found, read it in.
