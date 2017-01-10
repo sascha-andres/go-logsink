1ba367a994fcee626dd86cfeccb4a1a992b0e20b
Sascha Andres <sascha.andres@outlook.com>
2017-01-10 22:00:45 +0100

    Printing out address to connect

--- cmd/connect.go
+++ cmd/connect.go
@@ -15,6 +15,8 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/sascha-andres/go-logsink/client"
 	"github.com/spf13/cobra"
 )
@@ -26,7 +28,9 @@ var connectCmd = &cobra.Command{
 	Long: `This command is used to connect to a go-logsink server.
 Call it to forward data piped ito this application to the server.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		client.Connect(cmd.Flag("address").Value.String())
+		address := cmd.Flag("address").Value.String()
+		fmt.Printf("Connecting to %s\n", address)
+		client.Connect(address)
 	},
 }
 
