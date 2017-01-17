15e7d939c3a8b2ee0274396382c3243561b6fc03
Sascha Andres <sascha.andres@outlook.com>
2017-01-17 17:57:34 +0100

    Updated Static website

---
+++ www/js/main.js
@@ -0,0 +1,34 @@
+window.onload = function () {
+    var conn;
+    
+    var log = document.getElementById("log");
+    
+    function appendLog(item) {
+        var doScroll = log.scrollTop === log.scrollHeight - log.clientHeight;
+        log.appendChild(item);
+        if (doScroll) {
+            log.scrollTop = log.scrollHeight - log.clientHeight;
+        }
+    }
+    
+    if (window["WebSocket"]) {
+        conn = new WebSocket("ws://{{$}}/ws");
+        conn.onclose = function (evt) {
+            var item = document.createElement("div");
+            item.innerHTML = "<b>Connection closed.</b>";
+            appendLog(item);
+        };
+        conn.onmessage = function (evt) {
+            var messages = evt.data.split('\n');
+            for (var i = 0; i < messages.length; i++) {
+                var item = document.createElement("div");
+                item.innerText = messages[i];
+                appendLog(item);
+            }
+        };
+    } else {
+        var item = document.createElement("div");
+        item.innerHTML = "<b>Your browser does not support WebSockets.</b>";
+        appendLog(item);
+    }
+};
