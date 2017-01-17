25396408193c83b785f850b4ab0b7439e8660b52
Sascha Andres <sascha.andres@outlook.com>
2017-01-17 17:43:47 +0100

    Added context to Cobra command

--- cmd/web.go
+++ cmd/web.go
@@ -18,35 +18,27 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // webCmd represents the web command
 var webCmd = &cobra.Command{
 	Use:   "web",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Start a server instance with a web interface",
+	Long: `Use web to start a web server. Navigate with your favorite
+browser to localhost:8080 ( change the binding definition )
+to see the logs in your browser.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+  go-logsink web --serve ":80" --bind ":50051"`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: Work your own magic here
 		fmt.Println("web called")
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(webCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// webCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// webCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
+	webCmd.Flags().StringP("bind", "b", ":50051", "Provide bind definition")
+	webCmd.Flags().StringP("serve", "s", ":8080", "Provide bind definition")
+	viper.BindPFlag("web.bind", webCmd.Flags().Lookup("bind"))
+	viper.BindPFlag("web.serve", webCmd.Flags().Lookup("serve"))
 }
