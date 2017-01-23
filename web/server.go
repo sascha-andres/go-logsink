a0a28cec57b6df1cde12a990b3b3a7f3d8d0f0fb
Sascha Andres <sascha.andres@outlook.com>
2017-01-23 10:40:25 +0100

    Fixing division by zero for break at default

--- web/server.go
+++ web/server.go
@@ -22,9 +22,13 @@ type server struct {
 // SendLine implements logsink.SendLine
 func (s *server) SendLine(ctx context.Context, in *pb.LineMessage) (*pb.LineResult, error) {
 	breakAt := viper.GetInt("web.break")
-	iterations := int(len(in.Line) / breakAt)
-	for start := 0; start <= iterations; start++ {
-		s.hub.broadcast <- []byte(in.Line[start*breakAt : int32(math.Min(float64((start+1)*breakAt), float64(len(in.Line))))])
+	if breakAt == 0 {
+		s.hub.broadcast <- []byte(in.Line)
+	} else {
+		iterations := int(len(in.Line) / breakAt)
+		for start := 0; start <= iterations; start++ {
+			s.hub.broadcast <- []byte(in.Line[start*breakAt : int32(math.Min(float64((start+1)*breakAt), float64(len(in.Line))))])
+		}
 	}
 	return &pb.LineResult{Result: true}, nil
 }
