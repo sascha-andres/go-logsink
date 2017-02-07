2d80556d118a83bd9f59fe7b69eb9c07e8992fa4
Sascha Andres <sascha.andres@outlook.com>
2017-02-07 18:06:17 +0100

    Added lock file to listen command
    #16

--- cmd/listen.go
+++ cmd/listen.go
@@ -14,6 +14,10 @@
 package cmd
 
 import (
+	"fmt"
+	"log"
+
+	"github.com/nightlyone/lockfile"
 	"github.com/sascha-andres/go-logsink/server"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -26,6 +30,20 @@ var listenCmd = &cobra.Command{
 	Long: `This command is used to create a go-logsink server.
 Call it to have clients forward log messages here.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		if "" != viper.GetString("listen.lockfile") {
+			lock, err := lockfile.New(viper.GetString("listen.lockfile"))
+			if err != nil {
+				log.Fatal(err) // handle properly please!
+			}
+			err = lock.TryLock()
+
+			// Error handling is essential, as we only try to get the lock.
+			if err != nil {
+				log.Fatal(fmt.Errorf("Cannot lock %q, reason: %v", lock, err))
+			}
+
+			defer lock.Unlock()
+		}
 		server.Listen()
 	},
 }
@@ -33,5 +51,7 @@ Call it to have clients forward log messages here.`,
 func init() {
 	RootCmd.AddCommand(listenCmd)
 	listenCmd.Flags().StringP("bind", "b", ":50051", "Provide bind definition")
+	listenCmd.Flags().StringP("lockfile", "l", "", "Adhere to lockfile")
 	viper.BindPFlag("listen.bind", listenCmd.Flags().Lookup("bind"))
+	viper.BindPFlag("listen.lockfile", listenCmd.Flags().Lookup("lockfile"))
 }
